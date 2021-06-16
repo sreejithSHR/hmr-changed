09a006b5128bc9b6f8f5be831bda9909ebed4c2a
Sreejith.R <72004869+sreejithSHR@users.noreply.github.com>
2021-06-16 11:54:30 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -192,7 +192,7 @@ def main(img_path, json_path=None):
 def join_csv():
   path = 'hmr/output/csv/'                   
   all_files = glob.glob(os.path.join(path, "*.csv"))
-  all_files.sort(key=lambda x: int(x.split('/')[-1].split('.')[0]))
+  all_files.sort(key=lambda x: str(x.split("/")[-1].split("/")[0]))
   df_from_each_file = (pd.read_csv(f) for f in all_files)
   concatenated_df   = pd.concat(df_from_each_file, ignore_index=True)
 
